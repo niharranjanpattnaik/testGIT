0faf674940afe2c738667d3cb48fd1dac3db33c0
niharranjanpattnaik <niharranjanpattnaik@gmail.com>
2016-06-14 13:38:39 +0530

    added 2nd SOP
    added 2nd SOP

--- TestCode.java
+++ TestCode.java
@@ -1,1 +1,11 @@
+Class TestCode {
 
+	public TestCode () {
+	}
+
+	public static void main (String a[]) {
+		System.out.println ("first code on GIT");
+		System.out.println ("2nd changes"); 
+	}
+
+}
